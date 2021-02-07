9a7fbde2d591b2f59e60e7535a8c8c0289804fcc
Evgeniy Khlystov <hlystovea@gmail.com>
2021-02-07 18:31:08 +0700

    done

--- homework.py
+++ homework.py
@@ -1,21 +1,39 @@
+import logging
 import os
+import smtplib
 import time
+from email.message import EmailMessage
+from logging.handlers import RotatingFileHandler
 
 import requests
-import telegram
 from dotenv import load_dotenv
+from telegram import Bot
+
+handler = RotatingFileHandler('log.log', maxBytes=5000000, backupCount=2)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(funcName)s: %(message)s',
+    handlers=[handler],
+)
+
+logging.debug('Start script')
 
 load_dotenv()
 
 
-PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
+CHAT_ID = os.getenv('CHAT_ID')
+FROM_ADRESS = os.getenv('FROM_ADRESS')
+PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
+SMTP_LOGIN = os.getenv('SMTP_LOGIN')
+SMTP_PASS = os.getenv('SMTP_PASS')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TO_ADRESS = os.getenv('TO_ADRESS')
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    if homework.get('status') == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -23,30 +41,55 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {
+        'Authorization': f'OAuth {PRAKTIKUM_TOKEN}',
+    }
+    params = {
+        'from_date': current_timestamp,
+    }
+    homework_statuses = requests.get(URL, headers=headers, params=params)
+    homework_statuses.raise_for_status()
     return homework_statuses.json()
 
 
 def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+    logging.info(f'Попытка отправки сообщения: {message}')
+    return bot_client.send_message(CHAT_ID, message)
 
 
 def main():
-    # проинициализировать бота здесь
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    smtp_obj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp_obj.starttls()
+    smtp_obj.login(SMTP_LOGIN, SMTP_PASS)
+
+    bot_client = Bot(token=TELEGRAM_TOKEN)
+
+    current_timestamp = int(0)
+
+    current_error = None
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
-
-        except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
+                send_message(
+                    parse_homework_status(
+                        new_homework.get('homeworks')[0]), bot_client)
+            current_timestamp = new_homework.get('current_date', current_timestamp)
+            time.sleep(300)
+        except Exception as error:
+            print(f'Бот столкнулся с ошибкой: {repr(error)}')
+            logging.error(repr(error))
+            if not current_error == str(error):
+                current_error = str(error)
+                try:
+                    send_message(repr(error), bot_client)
+                except Exception as e:
+                    logging.error(repr(e))
+                    msg = EmailMessage()
+                    msg.set_content(f'{repr(error)}\n{repr(e)}')
+                    msg['Subject'] = 'api_sp1_bot'
+                    smtp_obj.send_message(msg, FROM_ADRESS, TO_ADRESS)
             time.sleep(5)
 
 
