fb9b3985e9e8db5e2d8ed6ebfd115d59a9834074
Evgeniy Khlystov <hlystovea@gmail.com>
2021-02-11 22:13:34 +0700

    fixed the reviewer's comments(2)

--- homework.py
+++ homework.py
@@ -4,12 +4,14 @@ import smtplib
 import time
 from email.errors import MessageError
 from email.message import EmailMessage
+from json import JSONDecodeError
 from logging.handlers import RotatingFileHandler
+from requests.exceptions import RequestException
 
 import requests
 import telegram
 from dotenv import load_dotenv
-from telegram import Bot
+from telegram import Bot, TelegramError
 
 rotate_file_handler = RotatingFileHandler(
     'log.log',
@@ -28,33 +30,31 @@ logging.debug('Start script')
 load_dotenv()
 
 
-CHAT_ID = os.environ.get('CHAT_ID')
-FROM_ADRESS = os.environ.get('FROM_ADRESS')
-PRAKTIKUM_TOKEN = os.environ.get('PRAKTIKUM_TOKEN')
-SMTP_LOGIN = os.environ.get('SMTP_LOGIN')
-SMTP_PASS = os.environ.get('SMTP_PASS')
-TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
-TO_ADRESS = os.environ.get('TO_ADRESS')
+CHAT_ID = os.environ['CHAT_ID']
+FROM_ADRESS = os.environ['FROM_ADRESS']
+PRAKTIKUM_TOKEN = os.environ['PRAKTIKUM_TOKEN']
+SMTP_LOGIN = os.environ['SMTP_LOGIN']
+SMTP_PASS = os.environ['SMTP_PASS']
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+TO_ADRESS = os.environ['TO_ADRESS']
 API_HW_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
+    statuses = {
+        'reviewing': 'Работа взята в ревью.',
+        'rejected': 'К сожалению в работе нашлись ошибки.',
+        'approved': ('Ревьюеру всё понравилось, '
+                     'можно приступать к следующему уроку.'),
+    }
     try:
         homework_name = homework.get('homework_name', 'Неизвестная работа')
-        if homework.get('status') == 'rejected':
-            verdict = 'К сожалению в работе нашлись ошибки.'
-        elif homework.get('status') == 'reviewing':
-            verdict = 'Работа взята в ревью.'
-        elif homework.get('status') == 'approved':
-            verdict = (
-                'Ревьюеру всё понравилось, '
-                'можно приступать к следующему уроку.'
-            )
-        else:
-            verdict = 'Статус неизвестен.'
+        verdict = statuses.get(homework.get('status'), 'Статус неизвестен.')
         return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
     except AttributeError as error:
-        logging.error(repr(error))
+        # на случай если функция используется где-то ещё и в неё
+        # может прилететь не словарь, а падать ей запрещено по ТЗ
+        logging.exception('')
         raise error
 
 
@@ -74,11 +74,9 @@ def get_homework_statuses(current_timestamp=0):
         # homework_statuses.raise_for_status()
         # ^-- тесты не пускают с этой строкой из-за имитации ответа сервера
         return homework_statuses.json()
-    except requests.exceptions.RequestException as error:
-        logging.error(repr(error))
-        raise error
-    except ValueError as error:
-        logging.error(repr(error))
+    except (RequestException, JSONDecodeError) as error:
+        # в учебных целях
+        logging.exception('')
         raise error
 
 
@@ -86,8 +84,9 @@ def send_message(message, bot_client):
     logging.info(f'Попытка отправки сообщения в Telegram. Текст: {message}')
     try:
         return bot_client.send_message(CHAT_ID, message)
-    except telegram.TelegramError as error:
-        logging.error(repr(error))
+    except TelegramError as error:
+        # в случае ошибки используем резервный канал связи
+        logging.exception('')
         message = (
             f'Бот столкнулся с ошибкой {repr(error)} '
             f'при отправке сообщения в Telegram.\n'
@@ -105,8 +104,9 @@ def send_mail(message, smtp_client):
         msg['Subject'] = 'api_sp1_bot'
         smtp_client.send_message(msg, FROM_ADRESS, TO_ADRESS)
         logging.info('Сообщение отправлено.')
-    except MessageError as error:
-        logging.error(repr(error))
+    except MessageError:
+        # только для логирования
+        logging.exception('')
 
 
 def main():
@@ -124,20 +124,23 @@ def main():
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('current_date'):
-                current_timestamp = new_homework['current_date']
+            current_timestamp = new_homework.get(
+                'current_date',
+                current_timestamp,
+            )
             if new_homework.get('homeworks'):
                 comment = parse_homework_status(new_homework['homeworks'][0])
                 send_message(comment, bot_client)
             time.sleep(300)
         except Exception as error:
-            logging.error(repr(error))
+            # для вызова send_message() и обработки собственных ошибок
+            logging.exception('')
             if not type(current_error) == type(error):
                 current_error = error
                 try:
                     send_message(repr(error), bot_client)
-                except Exception as error:
-                    logging.error(repr(error))
+                except (TelegramError, MessageError):
+                    # для продолжения работы при ошибке отправки сообщения
                     continue
             time.sleep(5)
 
